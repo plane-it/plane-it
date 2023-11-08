b34a923609ec65c510a05a2c381da9fcb3d0e166
AndreyR05 <Andrey.rodrigues@sptech.school>
2023-11-08 15:29:18 -0300

    feat: dashboard de historico diario

--- public/dashboard/screens/servidor/js/cronograma.js
+++ public/dashboard/screens/servidor/js/cronograma.js
@@ -1,9 +1,56 @@
 const dash = document.querySelector("#dash")
-
+const dashHist = document.querySelector("#dashHist")
 
 new Chart(dash, {
     type: 'bar',
     plugins: [ChartDataLabels],
+    data: {
+      labels: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: ['#dc3545','#ffc107', '#28a745'],
+        borderRadius: 10,
+        borderSkipped: false,
+        barPercentage: 0.5,
+      }]
+    },
+    options: {
+      grid:{
+        display: false,
+      },
+      scales: {
+        x: {
+          position: 'top',
+          grid: {
+            display: false,
+          },
+       },
+        y: {
+          display: false,
+          beginAtZero: true,
+          grid: {
+            display: false,
+          },
+        }
+      },
+      plugins: {
+        datalabels: {
+          color: "#fff",
+          font: {
+            size: 14,
+            weight: 'bold',
+          }
+        },
+        legend: {
+          display: false,
+        }
+      },
+    },
+})
+
+new Chart(dashHist, {
+    type: 'line',
     data: {
       labels: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'],
       datasets: [{
