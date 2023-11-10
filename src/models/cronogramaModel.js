f4ae99e94405d99e66b2c6cf0ae2ba5a478c565e
AndreyR05 <andrey.rodrigues@sptech.school>
2023-11-10 14:30:45 -0300

    feat: model do cronograma

--- src/models/cronogramaModel.js
+++ src/models/cronogramaModel.js
@@ -11,6 +11,17 @@ function getMedidaSemanal(idServidor, tipoComponente) {
 
     return database.executar(sql)
 }
+ 
+function getMediaDiaria(){
+    const sql = `
+        SELECT AVG(r.valor), DATE(r.dataHora) FROM tbRegistro r
+        JOIN tbComponente c ON c.idComp = r.fkComp
+        WHERE r.fkServidor = 1 AND DATE(r.datahora) = DATE("2023-11-09")
+        GROUP BY DATE(r.dataHora), c.fkTipoComponente;
+    `
+
+    return database.executar(sql)
+}
 
 module.exports = {
     getMedidaSemanal
