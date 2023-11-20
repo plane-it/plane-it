9385c30286c67fba002cf331a14db78ea090a323
AndreyR05 <andrey.rodrigues@sptech.school>
2023-11-19 22:52:25 -0300

    feat: add tabela de voos

--- src/database/planeit.sql
+++ src/database/planeit.sql
@@ -164,3 +164,32 @@ create table tbSpecs (
 	fkUnidadeMedida int,
 		foreign key (fkUnidadeMedida) references tbUnidadeMedida(idUnidadeMedida)	
 );
+
+create table voos(
+	id int primary key auto_increment,
+    siglaEmpresaAerea varchar(20),
+    nVoo varchar(20),
+    siglaAeroportoOrigem varchar(20),
+    horaPartidaPrevista datetime,
+    horaPartidaReal datetime,
+    siglaAeroportoDestino varchar(30),
+    horaChegadaPrevista datetime,
+    horaChegadaReal datetime,
+    situacao varchar(30)
+;
+
+drop procedure if exists deleteByMonth;
+DROP TEMPORARY TABLE if exists TEMP;
+DELIMITER $$
+CREATE PROCEDURE deleteByMonth(deleteYear int,deleteMonth INT)
+BEGIN 
+    CREATE TEMPORARY TABLE temp
+	SELECT id FROM voos
+	WHERE
+		(YEAR(horaPartidaPrevista) = deleteYear or YEAR(horaPartidaReal) = deleteYear) and
+		(MONTH(horaPartidaPrevista) = deleteMonth OR MONTH(horaPartidaReal) = deleteMonth);
+
+    DELETE FROM voos WHERE id IN (SELECT id FROM temp);
+
+    DROP TEMPORARY TABLE temp;
+END $$
